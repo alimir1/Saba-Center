1d3c5959efc797e152f2f877ff47174761d3f248
Ali Mir <110abidi@gmail.com>
2016-11-18 14:42:53 -0800

    minor edit to fix bug

--- Saba Center/ResidentAlimViewController.swift
+++ Saba Center/ResidentAlimViewController.swift
@@ -11,7 +11,15 @@ import MXParallaxHeader
 
 class ResidentAlimViewController: UIViewController {
     
+    let headerView = UIImageView()
+    
     let text = "Syed Nabi Raza Abidi was born and raised in Alipour, India into a religious, Syed family from both parents. After completing his early education at the age of 14, he moved to Iran to pursue further Islamic studies. He spent the first 2 years learning Farsi, basic Arabic grammar, and Ahkam in Najafabad, Isfahan, and then relocated to Qom for approximately 13 years. In Qom, he studied at renowned Islamic seminaries and institutes such as the Institute of Imam Jafar as-Sadiq under the guidance of Ayatollah Jafar Subhani while concurrently earning his PhD in Theology and Philosophy from a sister school at the University of Tehran. Upon completing his lower level studies, he began his higher hawzah education, Dars al-Kharij, under many reputable scholars, such as Ayatollah Fadhel Lankarani, Ayatullah Bahjat, and Ayatollah Jafer Subhani."
+    
+    var textForLabel: String? {
+        didSet {
+            aboutLabel.text = textForLabel!
+        }
+    }
     
     @IBOutlet weak var aboutLabel: UILabel!
     @IBOutlet weak var viewHeightConstraint: NSLayoutConstraint!
@@ -20,19 +28,21 @@ class ResidentAlimViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        aboutLabel.text = text
-        
-        viewHeightConstraint.constant = viewHeightConstraint.constant + aboutLabel.reqiuredHeight
 
-        let headerView = UIImageView()
+        textForLabel = text
+        configureScrollViewHeader()
+    }
+}
+
+extension ResidentAlimViewController {
+    func configureScrollViewHeader() {
+        viewHeightConstraint.constant = viewHeightConstraint.constant + aboutLabel.reqiuredHeight
         headerView.image = #imageLiteral(resourceName: "MoulanaAbidi")
         headerView.contentMode = .scaleAspectFill
         scrollView.parallaxHeader.view = headerView
         scrollView.parallaxHeader.height = 200
         scrollView.parallaxHeader.mode = .fill
         scrollView.parallaxHeader.minimumHeight = 0
-        
     }
 }
 
